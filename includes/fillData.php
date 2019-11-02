f0a7ce1dc9417f3a7d840a1f3d03a6d70db9b49b
wojmo <wojmoc96@gmail.com>
2019-11-02 23:58:53 +0100

    .

--- includes/fillData.php
+++ includes/fillData.php
@@ -5,26 +5,27 @@ if(isset($_SESSION['uID']) && $isDataFilled==0) {
     <div class="container">  
 <form id="contact" method="POST" action="index.php?action=fillData_send" accept-charset="character_set">
 <fieldset>
-      <input placeholder="Numer dowodu osobistego" name="nr_dowodu" type="text" tabindex="1" required autofocus>
+      <input placeholder="Numer dowodu osobistego*" name="nr_dowodu" type="text" tabindex="1" required autofocus>
 </fieldset>
 <fieldset>
-      <input placeholder="Numer karty kredytowej" name="nr_karty" type="text" tabindex="1" required autofocus>
+      <input placeholder="Numer karty kredytowej*" name="nr_karty" type="text" tabindex="1" required autofocus>
 </fieldset>
 <fieldset>
-      <input placeholder="Ulica" name="ulica" type="text" tabindex="1" required autofocus>
+      <input placeholder="Ulica*" name="ulica" type="text" tabindex="1" required autofocus>
 </fieldset>
 <fieldset>
-      <input placeholder="Miejscowość" name="miejscowosc" type="text" tabindex="1" required autofocus>
+      <input placeholder="Miejscowość*" name="miejscowosc" type="text" tabindex="1" required autofocus>
 </fieldset>
 <fieldset>
-      <input placeholder="Kod pocztowy" name="kod_pocztowy" type="text" tabindex="1" required autofocus>
+      <input placeholder="Kod pocztowy*" name="kod_pocztowy" type="text" tabindex="1" required autofocus>
 </fieldset>
 <fieldset>
-      <input placeholder="Numer mieszkania" name="numer_mieszkania" type="number" tabindex="1" required autofocus>
+      <input placeholder="Numer mieszkania" name="numer_mieszkania" type="number" tabindex="1">
 </fieldset>
 <fieldset>
-      <input placeholder="Numer domu" name="numer_domu" type="number" tabindex="1" required autofocus>
+      <input placeholder="Numer domu*" name="numer_domu" type="number" tabindex="1" required autofocus>
 </fieldset>
+<p style="color:red">* - pola obowiązkowe</p>
 <fieldset>
       <button name="fillData-submit" type="submit" id="contact-submit" data-submit="...Sending">Zatwierdź</button>
 </fieldset>
--- rejestracja.php
+++ rejestracja.php
@@ -4,32 +4,33 @@ echo '
 <div class="container">  
 <form id="contact" method="POST" action="index.php?action=register" accept-charset="character_set">
 <fieldset>
-      <input placeholder="Nazwa użytkownika" name="username" type="text" tabindex="1" required autofocus>
+      <input placeholder="Nazwa użytkownika*" name="username" type="text" tabindex="1" required autofocus>
 </fieldset>
 <fieldset>
-      <input placeholder="Hasło" name="password" type="password" tabindex="1" required autofocus>
+      <input placeholder="Hasło*" name="password" type="password" tabindex="1" required autofocus>
 </fieldset>
 <fieldset>
-      <input placeholder="Powtorz hasło" name="password-rpt" type="password" tabindex="1" required autofocus>
+      <input placeholder="Powtorz hasło*" name="password-rpt" type="password" tabindex="1" required autofocus>
 </fieldset>
 <fieldset>
-      <input placeholder="Adres e-mail" name="email" type="email" tabindex="1" required autofocus>
+      <input placeholder="Adres e-mail*" name="email" type="email" tabindex="1" required autofocus>
 </fieldset>
 <fieldset>
-      <input placeholder="Imie" name="imie" type="text" tabindex="1" required autofocus>
+      <input placeholder="Imie*" name="imie" type="text" tabindex="1" required autofocus>
 </fieldset>
 <fieldset>
-      <input placeholder="Nazwisko" name="nazwisko" type="text" tabindex="1" required autofocus>
+      <input placeholder="Nazwisko*" name="nazwisko" type="text" tabindex="1" required autofocus>
 </fieldset>
 <fieldset>
-      <input placeholder="Pesel" name="pesel" type="text" tabindex="1" required autofocus>
+      <input placeholder="Pesel*" name="pesel" type="text" tabindex="1" required autofocus>
 </fieldset>
 <fieldset>
-      <input placeholder="Numer telefonu" name="nr_tel" type="text" tabindex="1" required autofocus>
+      <input placeholder="Numer telefonu*" name="nr_tel" type="text" tabindex="1" required autofocus>
 </fieldset>
 <fieldset>
-      <input placeholder="Data urodzenia" name="data_ur" type="date" tabindex="1" required autofocus>
+      <input placeholder="Data urodzenia*" name="data_ur" type="date" tabindex="1" required autofocus>
 </fieldset>
+<p style="color:red">* - pola obowiązkowe</p>
 <fieldset>
       <button name="reg-submit" type="submit" id="contact-submit" data-submit="...Sending">Zatwierdź</button>
 </fieldset>
