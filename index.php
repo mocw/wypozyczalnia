ef04ca33776b6741f98468004bbde9cd1b4c10ae
Krzysztof Matykiewicz <frezz@MacBook-Pro-Krzysztof.local>
2019-11-05 15:33:35 +0100

    zmianaNazwy

--- index.php
+++ index.php
@@ -7,10 +7,7 @@ session_start();
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<<<<<<< HEAD
 <title>Wypożyczalnia</title>
-=======
->>>>>>> fce16fe3893780028791b00c32ce7488868dfd2b
 <script type="text/javascript" src="scripts/scripts.js"></script>
  <link rel="stylesheet" type="text/css" href="css/style.css" />
  <link rel="stylesheet" type="text/css" href="css/slider.css" />
