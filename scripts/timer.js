b6020e323981893ff534607402145ad8a9ce6e5d
wojmo <wojmoc96@gmail.com>
2019-12-01 03:48:25 +0100

    poprawki c.d

--- scripts/timer.js
+++ scripts/timer.js
@@ -22,7 +22,8 @@ function makeTimer() {
         if (seconds < "10") { seconds = "0" + seconds; }
           document.getElementById("timer"+i).innerHTML = "";
           if(days<0){
-            document.getElementById("timer"+i).innerHTML +="<div class=\"blink\">"+ days + "d " + hours + "h " + minutes + "m " + seconds + "s </div>";	       
+            if(days===-1) document.getElementById("timer"+i).innerHTML +="<div class=\"blink\">ZWROT DZISIAJ</div>";	       
+            else document.getElementById("timer"+i).innerHTML +="<div class=\"blink\">"+ days + "d " + hours + "h " + minutes + "m " + seconds + "s </div>";	       
           } 
           else {
             document.getElementById("timer"+i).innerHTML += days + "d " + hours + "h " + minutes + "m " + seconds + "s ";	       
