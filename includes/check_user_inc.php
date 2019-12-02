828f2c883fe32e5db1435381d17adc4daccb1aa7
wojmo <wojmoc96@gmail.com>
2019-12-02 23:09:49 +0100

    pop błędy

--- includes/check_user_inc.php
+++ includes/check_user_inc.php
@@ -3,7 +3,7 @@ require 'dbh.inc.php';
 if(isset($_SESSION['uID'])){
     if($_SESSION['id_klienta']==NULL 
     && $_SESSION['uUID']!='root') {
-       if($_GET['action']!='accountmgm' && $_GET['action']!='fillData' && $_GET['action']!='employeepanel') echo '<div class="alert alert-warning" role="alert"><a href="index.php?action=fillData">Uzupełnij swoje dane</a>, aby móc
+       if($_GET['action']==='oferta' || $_GET['action']==='home') echo '<div class="alert alert-warning" role="alert"><a href="index.php?action=fillData">Uzupełnij swoje dane</a>, aby móc
         zarezerwować pojazd!</div>';
         $isDataFilled=0;
     } else {
