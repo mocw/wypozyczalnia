d69c22e42af1578bc23cbcacbd577efae9b88ddd
wojmo <wojmoc96@gmail.com>
2019-12-10 14:54:27 +0100

    baza

--- home.php
+++ home.php
@@ -6,31 +6,10 @@
   <div class='slide4'></div>
   <div class='slide5'></div>
 </div>
-
-
-
-
-                    <!-- <a id="dropdownMenu1" data-toggle="dropdown" class=" dropdown-toggle">Zaloguj się</a>
-                    <ul class="dropdown-menu dropdown-menu-right mt-2">
-                       <li class="px-3 py-2">
-                           <form class="form" role="form" method="POST" action="index.php?action=login" id="logowanie">
-                                <div class="form-group">
-                                    <input id="userInput" placeholder="Nazwa użytkownika" class="form-control form-control-sm" type="text" required="" name="username">
-                                </div>
-                                <div class="form-group">
-                                    <input id="passwordInput" placeholder="Password" class="form-control form-control-sm" type="text" required="" name="password">
-                                </div>
-                                <div class="form-group">
-                                    <button type="submit" class="btn btn-primary btn-block" name="login-submit">Login</button>
-                                </div>
-                                <div class="form-group text-center">
-                                    <small><a href="#" data-toggle="modal" data-target="#modalPassword">Forgot password?</a></small>
-                                </div>
-                            </form>
-                        </li>
-                    </ul> -->
-      
-
-
-
-
+<div id="loginResult">
+<?php
+if(isset($_SESSION['uID'])){
+  echo 'Zalogowany!';
+}
+?>
+</div>
